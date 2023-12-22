8d155f088307a95b09ef81a6ddb4e5111fa9d8ea
Andrey Vasilev <ao.vasilev@gmail.com>
2023-12-22 17:39:22 +0300

    Initial Commit

---
+++ DebugYandexSmartCaptcha/DebugYandexSmartCaptchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebugYandexSmartCaptchaApp.swift
+//  DebugYandexSmartCaptcha
+//
+//  Created by Andrey Vasilev on 22.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct DebugYandexSmartCaptchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
